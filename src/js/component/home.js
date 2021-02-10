dc4a6f335322e1f69ceb444e472f3239c4d97f2f
Anmora96 <anmora.2396@gmail.com>
2021-02-10 05:33:49 +0000

    No entiendo nada

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,29 +6,10 @@ import board from "./board.js";
 import { Component } from "react";
 
 //create your first component
-export class Home extends Component {
-	render() {
-		return (
-			<div id="accordion" role="tablist">
-				<div className="card">
-					<div className="card-header" role="tab" id="headingOne">
-						<h5 className="mb-0">TIC TAC TOE</h5>
-					</div>
-
-					<div
-						id="collapseOne"
-						className="collapse show"
-						role="tabpanel"
-						aria-labelledby="headingOne">
-						<div className="card-body">
-							<Game />
-						</div>
-					</div>
-					<div className="card-footer" role="tab" id="headingTwo">
-						<h5 className="mb-0">Turnos</h5>
-					</div>
-				</div>
-			</div>
-		);
-	}
-}
+// export class Home extends Component {
+// 	render() {
+// 		return (
+			
+// 		);
+// 	}
+// }
